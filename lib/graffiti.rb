dac6f49b66f2b5f651c7b0e9d8bd0aaad2813f41
Darren Dao <darrendao@gmail.com>
2011-03-08 15:50:41 -0800

    Enforce uniqueness of name

--- lib/graffiti.rb
+++ lib/graffiti.rb
@@ -5,6 +5,7 @@ class Graffiti < ActiveRecord::Base
   validates_presence_of :value
   validates_presence_of :graffitiable_id
   validates_presence_of :graffitiable_type
+  validates_uniqueness_of :name, :scope => [:graffitiable_id, :graffitiable_type]
   def to_s
     name
   end
